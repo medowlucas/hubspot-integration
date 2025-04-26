13970d225072116e0e6a8c1e9f022b4c900293b3
Lucas Lopes De Souza Guilha <82731221+medowlucas@users.noreply.github.com>
2025-04-26 13:06:00 -0300

    feat: sets up first configurations and project building

---
+++ src/main/java/com/example/hubspotintegration/HubspotIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.example.hubspotintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubspotIntegrationApplication.class, args);
+    }
+}
